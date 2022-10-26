89026a1f6b2047111192cddcb0bd6d79120a92e8
ibrahim <bcelikel1994@gmail.com>
2022-10-26 18:37:24 +0300

    26-10-2022

--- src/test/java/Get01.java
+++ src/test/java/Get01.java
@@ -48,12 +48,31 @@ public class Get01 {
         // ii) Set the expected Data (beklenen datanin olusturulmasi, Post, Put, Patch)
         // Bizden post, put ya da patch istenmedigi icin bu case de kullanmayacagiz.
         // iii) Type code to send request ( Talep gondermek icin kod yazimi)
-        Response response=given().when().get(url);
+        Response response = given().when().get(url);
         response.prettyPrint();
 
         // iv) Do Assertion (dogrulama yapmak)
 
         response.then().assertThat().statusCode(200).contentType("application/json").statusLine("HTTP/1.1 200 OK");
+
+        // Status Code konsola yazdiralim
+        System.out.println("Status Code : "+response.getStatusCode());
+
+        // Content Type konsola yazdiralim
+        System.out.println("Content Type : "+ response.getContentType());
+
+        // Status Line konsola yaziralim
+        System.out.println("Status Line : "+response.getStatusLine());
+
+        // Header konsola yazdiralim
+        System.out.println("Header : "+response.getHeader("Server"));
+
+        // Headers konsola yazdiralim
+        System.out.println("Headers : "+response.getHeaders());
+
+        // Time konsola yazdiralim
+
+        System.out.println("Time : "+response.getTime());
 
     }
 
